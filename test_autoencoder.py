05ff3d5b1237b3bce4f241124e8b8ac6ed2f0922
Adam Good <dev.adamgood@gmail.com>
2019-12-30 21:54:21 -0600

    Automated some of testing

--- test_autoencoder.py
+++ test_autoencoder.py
@@ -1,15 +1,18 @@
+import argparse
 import torch
 from data_managment import get_dataloaders
 from matplotlib import pyplot as plt
-import argparse
 
-from basic_autoencoder import AutoEncoder, ConvAutoEncoder
+from simple_autoencoder import SimpleAutoEncoder
+from conv_autoencoder import ConvAutoEncoder
 
 def main():
     ''' main '''
     parser = argparse.ArgumentParser("Configure Testing of Auto Encoder")
+    parser.add_argument('-T', '--type', type=str, help='Type of autoencoder: simple, conv')
     parser.add_argument('-i', '--input', type=str, help="Name of File Containing the Model State")
     parser.add_argument('-bs', '--batchsize', type=int, default=1, help="Batch Size for Testing")
+    parser.add_argument('-o', '--output', type=str, default='out.png', help='Output Image')
     parser.add_argument('-S', '--seed', type=int, )
     args = parser.parse_args()
 
@@ -20,16 +23,19 @@ def main():
     batch_size = args.batchsize
     _, test_loader = get_dataloaders(batch_size, batch_size)
 
-    autoencoder = AutoEncoder([28*28, 512, 256, 128, 64, 32])
+    # autoencoder = None
+    if args.type == 'simple':
+        autoencoder = SimpleAutoEncoder()
+    elif args.type == 'conv':
+        autoencoder = ConvAutoEncoder()
+    else:
+        autoencoder = SimpleAutoEncoder()
     autoencoder.load_state_dict(torch.load(model_path))
-    convautoencoder = ConvAutoEncoder()
-    convautoencoder.load_state_dict(torch.load('./models/convautoencoder'))
     
     with torch.no_grad():
         batch, _ = next(iter(test_loader))
         x = batch
         y = autoencoder(x)
-        y2 = convautoencoder(x)
 
         if batch_size > 1:
             figure, ax = plt.subplots(batch_size, 2)
@@ -54,35 +60,8 @@ def main():
             figure.colorbar(rcm, ax=ax[1], shrink=0.5)
 
         plt.tight_layout()
-        plt.savefig(f'output/out.png')
-        plt.clf()
-        # plt.show()
-
-        if batch_size > 1:
-            figure, ax = plt.subplots(batch_size, 2)
-            for i, (orig, recon) in enumerate(zip(x,y2)):
-                ocm = ax[i, 0].imshow(orig.squeeze(), cmap='gray')
-                ax[i, 0].set_title("Original")
-
-                rcm = ax[i, 1].imshow(recon.squeeze(), cmap='gray')
-                ax[i, 1].set_title("Reconstruction")
-
-                figure.colorbar(ocm, ax=ax[i, 0], shrink=0.5)
-                figure.colorbar(rcm, ax=ax[i, 1], shrink=0.5)
-        elif batch_size == 1:
-            figure, ax = plt.subplots(batch_size, 2)
-            ocm = ax[0].imshow(x.squeeze(), cmap='gray')
-            ax[0].set_title("Original")
-
-            rcm = ax[1].imshow(y2.squeeze(), cmap='gray')
-            ax[1].set_title("Reconstruction")
-
-            figure.colorbar(ocm, ax=ax[0], shrink=0.5)
-            figure.colorbar(rcm, ax=ax[1], shrink=0.5)
-
-        plt.tight_layout()
-        plt.savefig(f'output/convout.png')
-        plt.clf()
+        plt.savefig(f'output/{args.output}')
+        plt.show()
 
 if __name__ == '__main__':
     main()
