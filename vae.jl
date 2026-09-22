a61cce383cfe0c71a7d83e775f459ce9d197e145
Adam Good <dev.adamgood@gmail.com>
2026-09-21 21:31:27 -0400

    added plotData

--- vae.jl
+++ vae.jl
@@ -1,18 +1,21 @@
-using Base: dataids
-
-
 using Flux
 using MLDatasets
 using Statistics
 using ProgressMeter
+using Plots
 
 function getData()
     data = rand(Float32, 2, 1000)
-    truth = [xor(col[1] > 0.5, col[2] > 0.5 for col in eachcol(data))]
+    classes = [xor(col[1] > 0.5, col[2] > 0.5) for col in eachcol(data)]
     return (
         data,
-        truth
+        classes
     )
+end
+
+function plotData(datapoints::Matrix, classes::Vector{Bool})
+    x,y = eachrow(datapoints) 
+    scatter(x,y, zcolor=classes)
 end
 
 function newModel()
@@ -22,3 +25,5 @@ function newModel()
         Dense(3 => 2)
     )
 end
+
+
