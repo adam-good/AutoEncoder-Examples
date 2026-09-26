6c2361d8d47978d9ffca30b4a91e6f64ed152e61
Adam Good <dev.adamgood@gmail.com>
2026-09-25 20:10:18 -0600

    reafactor

--- autoencoders/basic_nn.jl
+++ autoencoders/basic_nn.jl
@@ -3,46 +3,6 @@ using Flux.Optimisers
 using Flux.Losses
 using MLUtils
 using ProgressMeter
-using Plots
-
-xor2d(x::Number, y::Number)::Bool = xor(x > 0.5, y > 0.5)
-
-struct DataObservation
-    x::Float32
-    y::Float32
-    class::Bool
-end
-struct DataSample
-    N::Integer
-    features::Matrix{Float32}
-    classes::Vector{Bool}
-end
-length(x::DataSample) = x.N
-features(x::DataSample) = x.features
-classes(x::DataSample) = x.classes
-# TODO: These should error on overflow
-ith_feature(i::Integer, x::DataSample) = eachcol(x.features)[i] 
-ith_class(i::Integer, x::DataSample) = x.classes[i] 
-ith_observation(i::Integer, x::DataSample) = begin 
-    x,y = ith_feature(i,x)
-    DataObservation(x,y,ith_class(i,x))
-end
-
-function get_data(N::Integer=1000)::DataSample
-    data = rand(Float32, 2, N)
-    DataSample(
-        N,
-        data, 
-        [xor2d(col[1], col[2]) for col in eachcol(data)]
-    )
-end
-
-function plot_data(sample::DataSample)
-    x, y = eachrow(features(sample))
-    scatter(x, y, marker_z=classes(sample),
-        xlims=(0, 1),
-        ylims=(0, 1))
-end
 
 # TODO: Add some parameters as args so this is more flexible
 function new_model()
@@ -53,11 +13,11 @@ function new_model()
     )
 end
 probs(model, data) = model(data) |> softmax
-predict(probs) = probs |> eachcol .|> argmax
+predict(probs) = probs |> eachcol .|> argmax .|> x -> x - 1
 
 struct ModelTrainer
-    model::Flux.Chain
-    opt_state::Optimisers
+    model       # TODO: What type should this be
+    opt_state   # TODO: What type should this be
 end
 function new_trainer(model, opt)
     ModelTrainer(model, Flux.setup(opt, model))
@@ -71,15 +31,12 @@ function update!(trainer, x, y)
     loss
 end
 
-function train!(model, dataloader, epochs)
-    opt_state = Flux.setup(Optimisers.Adam(0.01), model)
+function train!(trainer, dataloader, epochs)
     losses = []
-    @showprogress for epoch in 1:epochs
+    @showprogress for _ in 1:epochs
         for (x, y) in dataloader
-            loss = update!(model, opt_state, x, y)
+            loss = update!(trainer, x, y)
             push!(losses, loss)
         end
     end
-
-    opt_state
 end
---
+++ autoencoders/data.jl
@@ -0,0 +1,40 @@
+
+using Plots
+
+xor2d(x::Number, y::Number)::Bool = xor(x > 1.5, y > 0.5)
+
+struct DataSample
+    N::Integer
+    features::Matrix{Float32}
+    classes::Vector{Bool}
+end
+length(x::DataSample) = x.N
+features(x::DataSample) = x.features
+classes(x::DataSample) = x.classes
+ith_feature(i::Integer, x::DataSample) = eachcol(x.features)[i]
+ith_class(i::Integer, x::DataSample) = x.classes[i]
+
+function new_sample(N::Integer=1000)::DataSample
+    data = rand(Float32, 2, N)
+    DataSample(
+        N,
+        data,
+        [xor2d(col[1], col[2]) for col in eachcol(data)]
+    )
+end
+
+function dataloader(data::DataSample)
+    Flux.DataLoader(
+        (data.features, Flux.onehotbatch(data.classes, [true, false])),
+        batchsize=64, shuffle=true
+    )
+end
+
+function plot_data(features, classes)
+    x, y = eachrow(features)
+    scatter(x, y, marker_z=classes,
+        xlims=(0, 1),
+        ylims=(0, 1))
+end
+
+
